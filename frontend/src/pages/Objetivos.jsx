97ed4915ff98685809fb84d6bef3d9bc00c6322f
jnassula <jonatanassula@gmail.com>
2021-07-19 15:50:21 +0100

    updating css

--- frontend/src/pages/Objetivos.jsx
+++ frontend/src/pages/Objetivos.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import styles from '../styles/Objetivos.module.css';
 import { useParams } from 'react-router';
 import { useHistory } from 'react-router';
+import * as BiIcons from "react-icons/bi";
 
 
 function Objetivo() {
@@ -59,45 +60,53 @@ function Objetivo() {
   if (!concluido) {
 
     return (
-      <div className="Objetivo">
-        {/* <button onClick={() => console.log(listaDeObjetivos)}> consolelog </button> */}
-        <button
-          onClick={() => wizardObjetivo()}
-        > Criar Objetivo
-        </button>
-
-        <h1>Objetivos</h1>
+      <div className={styles.Objetivos}>
+        <div className={styles.bgWizard}>
+          <button
+              onClick={() => wizardObjetivo()}
+          > 
+            <BiIcons.BiPlus />
+            Criar Objetivo
+          </button>
+          <h1>Objetivos</h1>
+        </div>
         {
           listaDeObjetivos?.map(lista => (
-            <li key={lista._id}>
-              <p> O seu objetivo é {lista.objetivo} </p>
-              <p> A data limite é {lista.prazo} </p>
-              <p> O valor a atingir é {lista.valorAtingir} </p>
-              <p> Está a {lista.qtdContribuicoes === 1 ? 1 + " contribuição" : `${lista.qtdContribuicoes} contribuições`} de atingir o seu objetivo! </p>
-
-              <button
-                onClick={() => deleteObjetivo(lista._id)}
-              >
-                Apagar
-              </button>
-              <button onClick={() => contribuirObjetivo(lista._id)} type="submit">Contribuir {lista.valorContribuicoes} €</button>
-            </li>
+            <div className={styles.card}>
+                <li key={lista._id}>
+                <p> O seu objetivo é {lista.objetivo} </p>
+                <p> A data limite é {lista.prazo} </p>
+                <p> O valor a atingir é {lista.valorAtingir} </p>
+                <p className={styles.pObjetivos}> Está a {lista.qtdContribuicoes === 1 ? 1 + " contribuição" : `${lista.qtdContribuicoes} contribuições`} de atingir o seu objetivo! </p>
+                <div className={styles.btnObjetivos}>
+                  <button className={styles.btnContribuir} onClick={() => contribuirObjetivo   (lista._id)} type="submit">Contribuir {lista.valorContribuicoes} €
+                  </button>
+                  <button className={styles.deleteObjetivos}
+                    onClick={() => deleteObjetivo(lista._id)}
+                  >
+                    Apagar
+                  </button>
+                </div>
+              
+              </li>
+            </div>
+            
           ))
         }
-        <br />
-        <div>
-          <p>
-            <button type="submit">Contribuir</button>
-          </p>
-        </div>
+        
+        
       </div>
     );
   } else {
     setTimeout(() => {
       updateConcluido(false)
-    }, 2200);
+    }, 3200);
     return (
-      <h1> Parabéns! Concluiu o seu objetivo! </h1>
+      <div className={styles.congratsWizard}>
+        <h1> Parabéns! Concluiu o seu objetivo! </h1>
+        <img src="../docs/imagens/congratulations.gif" />
+      </div>
+      
     )
   }
 }
