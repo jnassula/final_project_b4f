b2eec473c652a4724754651bb9cd9f8b29b30a60
jnassula <jonatanassula@gmail.com>
2021-07-19 09:52:30 +0100

    css initial page

--- frontend/src/pages/Primeira.jsx
+++ frontend/src/pages/Primeira.jsx
@@ -1,18 +1,19 @@
-
-import { Link } from "react-router-dom"
 import { Formik, Field } from 'formik';
+import styles from '../styles/Primeira.module.css'
 
 
 function Primeira({setLoggedIn}){
     return(
-        <div>
-        <Formik>
-             <form>
-              <Field name="pessoa" type="text" placeholder="Insira o seu nome" required />
-            </form>
-    
-    </Formik><button onClick={() => setLoggedIn(true)}>Entrar</button>
-    </div>
+        <div className={styles.PaginaInicial}>
+            <img src="../docs/imagens/small_logo_ss.png" />
+            <img className={styles.BigLogo} src="../docs/imagens/big_logo_ss.png" />
+            <Formik>
+                <form>
+                    <Field name="pessoa" type="text" placeholder="Insira o seu nome" required />
+                </form>
+            </Formik>
+            <button onClick={() => setLoggedIn(true)}>Entrar</button>
+        </div>
     
     )}
 
